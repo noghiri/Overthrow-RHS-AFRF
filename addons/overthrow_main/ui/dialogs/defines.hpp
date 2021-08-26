5b743642d859427128ce02d8c07633ac49312320
guilemouse <dahitman101@gmail.com>
2021-08-25 20:24:53 -0400

    Fixed missing RscEditOverthrow
    Didn't copy nifnat correctly. Now fixed.

--- addons/overthrow_main/ui/dialogs/defines.hpp
+++ addons/overthrow_main/ui/dialogs/defines.hpp
@@ -106,7 +106,76 @@ class RscControlsGroup;
 
 ///////////////////////////////////////////////////////////////////////////
 /// Base Classes
+/// Nifnats RSCEditOverthrow added.
 ///////////////////////////////////////////////////////////////////////////
+class RscEditOverthrow
+{
+	deletable = 0;
+	fade = 0;
+	access = 0;
+	type = 2;
+	x = 0;
+	y = 0;
+	h = 0.04;
+	w = 0.2;
+	colorBackground[] =
+	{
+		0,
+		0,
+		0,
+		1
+	};
+	colorText[] =
+	{
+		0.95,
+		0.95,
+		0.95,
+		1
+	};
+	colorDisabled[] =
+	{
+		1,
+		1,
+		1,
+		0.25
+	};
+	colorSelection[] =
+	{
+		"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.69])",
+		"(profilenamespace getvariable ['GUI_BCG_RGB_G',0.75])",
+		"(profilenamespace getvariable ['GUI_BCG_RGB_B',0.5])",
+		1
+	};
+	autocomplete = "";
+	text = "";
+	size = 0.2;
+	style = "0x00 + 0x40";
+	font = "PuristaMedium";
+	shadow = 2;
+	sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * (0.25 + (0.3 / (pixelGrid / 14))))";
+	canModify = 1;
+	tooltipColorText[] =
+	{
+		1,
+		1,
+		1,
+		1
+	};
+	tooltipColorBox[] =
+	{
+		1,
+		1,
+		1,
+		1
+	};
+	tooltipColorShade[] =
+	{
+		0,
+		0,
+		0,
+		0.65
+	};
+};
 class RscOverthrowText
 {
 	deletable = 0;
